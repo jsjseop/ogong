c62ffeaf163b631d6115d9281570dcd8ddadf14d
jkj7651 <57559288+othertkfka@users.noreply.github.com>
2021-07-12 00:52:34 +0900

    Update OgongApplication.java

--- ogong/src/main/java/com/ogong/OgongApplication.java
+++ ogong/src/main/java/com/ogong/OgongApplication.java
@@ -3,7 +3,9 @@ package com.ogong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class OgongApplication {
 
