c68aabe138e76fbdca4ef72343c0b0e3d726f6ad
user <user@DESKTOP-04GIHST>
2021-07-13 11:52:31 +0900

    지훈 : 오류 뭐지

--- ogong/src/main/java/com/ogong/service/admin/AdminService.java
+++ ogong/src/main/java/com/ogong/service/admin/AdminService.java
@@ -1,5 +1,6 @@
 package com.ogong.service.admin;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.ogong.service.domain.User;
@@ -9,29 +10,11 @@ import com.ogong.service.domain.Report;
 
 public interface AdminService {
 
-	// 신고된 사용자 조회
-	public Map<String, Object> listReportUser(Search search) throws Exception;
-	
-	// 신고된 게시글 조회
-	public Map<String, Object> listReportBoard(Search search) throws Exception;
-	
-	// 신고된 댓글 조회
-	public Map<String, Object> listReportComment(Search search) throws Exception;
-	
-	// 신고된 답변 조회
-	public Map<String, Object> listReportAnswer(Search search) throws Exception;
+	// 신고 목록 조회
+	public Map<String, Object> getlistReport(HashMap<String, Object> map) throws Exception;
 	
 	// 모든 회원 조회
-	public Map<String, Object> listTotalUser(Search search) throws Exception;
-	
-	// 모든 탈퇴회원 조회
-	public Map<String, Object> listWithdrawUser(Search search) throws Exception;
-	
-	// 모든 복구회원 조회
-	public Map<String, Object> listRestoreUser(Search search) throws Exception;
-	
-	// 모든 정지회원 조회
-	public Map<String, Object> listSuspendUser(Search search) throws Exception;
+	public Map<String, Object> getlistTotalUser(HashMap<String, Object> map) throws Exception;
 	
 	// 회원 정지하기
 	public void updateUserSuspend(Report report) throws Exception;
--- ogong/src/main/java/com/ogong/service/integration/impl/IntegrationServiceImpl.java
+++ ogong/src/main/java/com/ogong/service/integration/impl/IntegrationServiceImpl.java
@@ -1,7 +1,118 @@
 package com.ogong.service.integration.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ogong.common.Search;
+import com.ogong.service.domain.Message;
+import com.ogong.service.domain.Notice;
+import com.ogong.service.domain.User;
+import com.ogong.service.integration.IntegrationMapper;
 import com.ogong.service.integration.IntegrationService;
 
+@Service
 public class IntegrationServiceImpl implements IntegrationService {
+	
+	@Autowired
+	IntegrationMapper integrationMapper;
+
+	@Override
+	public Map<String, Object> getlistReceiveMessage(HashMap<String, Object> map) throws Exception {
+		
+		List<Message> list = integrationMapper.getlistReceiveMessage(map);
+		int totalCount = integrationMapper.getTotalCount(map);
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		map.put("list", list);
+		map.put("totalCount", new Integer(totalCount));
+		
+		return result;
+	}
+
+	@Override
+	public Map<String, Object> getlistSendMessage(HashMap<String, Object> map) throws Exception {
+		List<Message> list = integrationMapper.getlistSendMessage(map);
+		int totalCount = integrationMapper.getTotalCount2(map);
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		map.put("list", list);
+		map.put("totalCount", new Integer(totalCount));
+		
+		return result;
+	}
+
+	@Override
+	public void addSendMessage(Message message) throws Exception {
+
+		integrationMapper.addSendMessage(message);
+	}
+
+	@Override
+	public void deleteMessage(int[] messageNo) throws Exception {
+
+		integrationMapper.deleteMessage(messageNo);
+	}
+
+	@Override
+	public void addNotice(Notice notice) throws Exception {
+		// TODO Auto-generated method stub
+		integrationMapper.addNotice(notice);
+	}
+
+	@Override
+	public Map<String, Object> listLearningHistoryRanking() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	@Override
+	public Map<String, Object> listBananaRanking(HashMap<String, Object> map) throws Exception {
+		
+		List<User> list = integrationMapper.listBananaRanking(map);
+		
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		map.put("list", list);
+		
+		
+		return result;
+	}
+
+	@Override
+	public Map<String, Object> listChooseCountRanking(HashMap<String, Object> map) throws Exception {
+		List<User> list = integrationMapper.listChooseCountRanking(map);
+		
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		map.put("list", list);
+		
+		
+		return result;
+	}
+
+	@Override
+	public Notice getlistNotice(String email) throws Exception {
+
+		return integrationMapper.getlistNotice(email);
+	}
+
+	@Override
+	public void deleteNotice(int noticeNo) throws Exception {
+
+		integrationMapper.deleteNotice(noticeNo);
+	}
+
+	@Override
+	public void deleteAllNotice(String email) throws Exception {
+
+		integrationMapper.deleteAllNotice(email);
+	}
+	
+	
 
 }
